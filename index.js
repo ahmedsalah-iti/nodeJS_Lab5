036888b1c311026106735c6de56640e2a571405b
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-05-08 21:04:04 +0300

    fininsh lab5

--- index.js
+++ index.js
@@ -8,6 +8,44 @@ const usersRoutees = require('./routes/users')
 const dotenv = require('dotenv');
 const AppError = require('./utils/AppError')
 const catchAsync = require('./utils/catchAsync')
+const multer = require('multer');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        // console.log("\n================\n",file,"\n================\n")
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage ,
+    fileFilter: function(req,file,cb){
+        // console.log("\n================\n",file,"\n================\n")
+        /*
+        ================
+ {
+  fieldname: 'avatar',
+  originalname: '8532b623f7aeaba917d1ae9cbd670b20.jpg',
+  encoding: '7bit',
+  mimetype: 'image/jpeg'
+}
+================
+        */
+const allowedTypes = ['image/jpeg', 'image/jpg', 'image/gif', 'image/png'];
+if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+}else{
+    cb(new AppError(403,'Only JPG, JPEG, GIF, and PNG files are allowed'), false);
+}
+    }
+ })
+
+
+
 dotenv.config();
 mongoose.connect('mongodb://127.0.0.1:27017/tododay5').then(()=>{
     console.log("connected successfuly to the database.")
@@ -17,6 +55,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/tododay5').then(()=>{
 app.use(express.json())
 app.use('/todos',todosRoutees)
 app.use('/users',usersRoutees)
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    res.status(201).json({status:"success"})
+  })
 app.use((req,res,next)=>{
 next(new AppError(404,"page not found."))
 });
